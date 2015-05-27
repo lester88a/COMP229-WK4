cfb1c1aabf8a8e8249045a551965af13e93ee0ca
lester88a <lester88a@gmail.com>
2015-05-27 13:43:13 -0400

    myScript

--- myForm.aspx.cs
+++ myForm.aspx.cs
@@ -9,6 +9,6 @@ public partial class myForm : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        FormSubmited.Text = "Thank you, " + Request.Form["firstName"] + " " + Request.Form["lassttName"] + "!";
     }
 }
---
+++ myScript.js
@@ -0,0 +1,18 @@
+﻿function validateForm()
+{
+    var result = true; //assume the form is correct
+    var form = document.getElementById("theForm");
+    var values = "";
+    for (var i = 0; i < form.elements.length; i++)
+    {
+        values += "field[" + i + "] =" + form.elements[i].value + "\n";
+    }
+    alert(values);
+
+    var firstName = form.elements[0];
+    if ((firstName == null) || (firstName == ""))
+    {
+        alert("First name cannot be empty!")
+        result = false;
+    }
+}
